62c117a72f6fc203c4ca24a3fbb51bfadcc74328
qubiak <kubiak1986@gmail.com>
2021-03-24 11:23:52 +0100

    start

---
+++ src/main/java/po/qubiak/weather/map/WeatherMapApplication.java
@@ -0,0 +1,13 @@
+package po.qubiak.weather.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/po/qubiak/weather/map/WeatherMapApplicationTests.java
@@ -0,0 +1,13 @@
+package po.qubiak.weather.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
